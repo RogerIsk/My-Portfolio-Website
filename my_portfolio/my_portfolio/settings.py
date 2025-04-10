23c505c6feb0a3f7cfdefdbcbabf002cbf6014d9
varko <varko@example.com>
2025-04-10 19:40:20 +0200

    Installed my database - PostgreSQL. Added it in settings.py. Created a PostgreSQL user and added all privileges to it for the newly created database

--- my_portfolio/my_portfolio/settings.py
+++ my_portfolio/my_portfolio/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'my_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'my_portfolio_db',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
