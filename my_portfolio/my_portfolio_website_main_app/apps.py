0556ca5f887e754f024272ad9fb73240ffac8640
varko <varko@example.com>
2025-04-10 19:21:58 +0200

    Created my project and main app that will be respoonsible for the website that will serve as my portfolio

---
+++ my_portfolio/my_portfolio_website_main_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyPortfolioWebsiteMainAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'my_portfolio_website_main_app'
